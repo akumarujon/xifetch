8a521f5e978a23656d0676128f08728b375a2dbc
akumawu <akumaru.senju@gmail.com>
2022-10-17 21:00:22 +0500

    disk info added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+from dis import dis
 import platform
 import os
 import psutil
@@ -8,6 +9,8 @@ import psutil
 # gpu info
 gpu_unix_command = "lspci | grep VGA"
 cpu_unix_command = "cat /proc/cpuinfo | grep name"
+
+disk_info = psutil.disk_usage('/')
 
 result = os.popen(gpu_unix_command).readlines()
 cpu_info = os.popen(cpu_unix_command).read().split('\n')
@@ -32,6 +35,7 @@ fetch = f"""        OS: {os_name}
         DE: {os.environ.get("DESKTOP_SESSION")}
         CPU: {cpu_info[0].split(':')[1]}
         GPU: {gpus}
+        Disk: {round(disk_info.used/(1028**3))} GiB / {round(disk_info.total/(1028*1028*1028))} GiB
         Memory: {memory['used']} MiB / {memory['total']} MiB"""
 
 print(fetch)
